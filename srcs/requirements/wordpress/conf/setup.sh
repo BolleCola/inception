32c4d31f3cb8a497c9a3bd766b81e8637f56eba8
BolleCola <tpaquier@student.42lausanne.ch>
2023-03-09 15:03:10 +0100

    23soleil@

--- srcs/requirements/wordpress/conf/setup.sh
+++ srcs/requirements/wordpress/conf/setup.sh
@@ -1,2 +1,7 @@
 # get wordpress et get wp-cli
 
+wp core download --path= --locale=fr_FR
+cd #into le path du download
+wp config create --dbname= --dbuser= --dbpass= 
+wp db create
+wp core install --url=tpaquier.42.fr --title=tpaquier --admin_user= --admin_password= --locale=fr_FR --skip-emal

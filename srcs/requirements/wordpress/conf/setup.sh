4cccb7b265551b00ea2869da34b012b38b6131ac
BolleCola <tpaquier@student.42lausanne.ch>
2023-03-23 16:42:25 +0100

    pf: remplie de bug 100%

--- srcs/requirements/wordpress/conf/setup.sh
+++ srcs/requirements/wordpress/conf/setup.sh
@@ -1,7 +1,13 @@
 # get wordpress et get wp-cli
 
-wp core download --path= --locale=fr_FR
+while (!mysqladmin ping -h "db") do
+    sleep 1;
+done
+
+wp core download --path=/home/wordpress --locale=fr_FR
+#check si le folder wordpress est install
 cd #into le path du download
-wp config create --dbname= --dbuser= --dbpass= 
+wp config create --dbname=${MDB_DATABASE} --dbuser=${MDB_USER} --dbpass=${MDB_PASSWORD} 
+# attendre wp-config.php qui est cree donc cheker si bien cree
 wp db create
-wp core install --url=tpaquier.42.fr --title=tpaquier --admin_user= --admin_password= --locale=fr_FR --skip-emal
+wp core install --url=${DOMAIN_NAME} --title=${LOGIN} --admin_user=${WP_ADMIN} --admin_password=${WP_ADMIN_PASSWORD} --locale=fr_FR --skip-email

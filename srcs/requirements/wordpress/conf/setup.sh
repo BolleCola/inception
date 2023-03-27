c85483961a729de6a4ded8534e8eb27029372f96
Thibaud Paquier <TP.Titus@hotmail.fr>
2023-03-27 11:50:51 +0200

    123soleil

--- srcs/requirements/wordpress/conf/setup.sh
+++ srcs/requirements/wordpress/conf/setup.sh
@@ -1,13 +1,23 @@
 # get wordpress et get wp-cli
 
 while (!mysqladmin ping -h "db") do
-    sleep 1;
+    sleep 1
 done
 
 wp core download --path=/home/wordpress --locale=fr_FR
+
 #check si le folder wordpress est install
+while (! -f "wp-config-sample.php") do
+    sleep 1
+done
 cd #into le path du download
-wp config create --dbname=${MDB_DATABASE} --dbuser=${MDB_USER} --dbpass=${MDB_PASSWORD} 
+wp config create --dbname=${MDB_DATABASE} --dbuser=${MDB_USER} --dbpass=${MDB_PASSWORD}
+
 # attendre wp-config.php qui est cree donc cheker si bien cree
+while (! -f "wp-config.php") do
+    sleep 1
+done
+
 wp db create
 wp core install --url=${DOMAIN_NAME} --title=${LOGIN} --admin_user=${WP_ADMIN} --admin_password=${WP_ADMIN_PASSWORD} --locale=fr_FR --skip-email
+# wp user create 

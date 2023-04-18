513ad57df23406cab7f9bc1c9a2af1d1e865ac1c
BolleCola <tpaquier@student.42lausanne.ch>
2023-04-18 14:22:57 +0200

    123

--- srcs/requirements/mariadb/conf/database.sh
+++ srcs/requirements/mariadb/conf/database.sh
@@ -1,15 +1,19 @@
-mariadb-install-db --user=mysql --datadir=/var/lib/mysql --auth-root-authentication-method=normal
 
-chown -R mysql:mysql /var/lib/mysql
-chown -R mysql:mysql /run/lib/mysqld
+if test -f ".setup"; then
+    echo "DATABASE already create !!!"
+else
+    mariadb-install-db --datadir=/var/lib/mysql --auth-root-authentication-method=normal
 
-mysqld --datatdir=/var/lib/mysql & #tache de fond
+    chown -R mysql:mysql /var/lib/mysql
 
-while ! mysqladmin ping -h "mariadb" --silent; do
-    sleep 1
-done
+    mysqld --datatdir=/var/lib/mysql & #tache de fond
 
-eval "echo \"$(cat /dbase.sql)\"" | mariadb
-touch .setup
+    while ! mysqladmin ping -h "mariadb" --silent; do
+        sleep 1
+    done
+
+    eval "echo \"$(cat /dbase.sql)\"" | mariadb
+    touch .setup
+fi
 
 mysqld_safe --datadir=/var/lib/mysql

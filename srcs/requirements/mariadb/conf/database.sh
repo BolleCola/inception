2e5bad6b8bd77b812d29fb15c7543c6fc06c030c
Thibaud Paquier <TP.Titus@hotmail.fr>
2023-05-23 14:53:10 +0200

    123soleil

--- srcs/requirements/mariadb/conf/database.sh
+++ srcs/requirements/mariadb/conf/database.sh
@@ -1,17 +1,18 @@
 cat .dbexist 2>/dev/null
 if [ $? -ne 0 ]; then
-    echo "DATABASE already create !!!"
-else
+    touch .dbexist
+
     mariadb-install-db --datadir=/var/lib/mysql --auth-root-authentication-method=normal
 
     chown -R mysql:mysql /var/lib/mysql
     chown -R mysql:mysql run/mysqld
 
-    mysqld_safe --datatdir=/var/lib/mysql & #tache de fond
-    while [! mysqladmin ping -h "mariadb" --silent]; do
+    mysqld_safe --datadir=/var/lib/mysql & #tache de fond
+    while ! mysqladmin ping -h "mariadb" --silent; do
         sleep 1
     done
     eval "echo \"$(cat /tmp/dbase.sql)\"" | mariadb
-    touch .dbexist
+else
+   echo "DATABASE already create !!!"
 fi
 mysqld_safe --datadir=/var/lib/mysql
--- srcs/requirements/wordpress/conf/setup.sh
+++ srcs/requirements/wordpress/conf/setup.sh
@@ -1,36 +1,22 @@
-# get wordpress et get wp-cli
-# Check if listen exists in the file
-target="/etc/php7/php-fpm.d/www.conf"
-
-grep -E "listen = 127.0.0.1" $target >/dev/null 2>&1
-
-# configure www.conf
-if [ $? -eq 0 ]; then
-  sed -i "s|.*listen = 127.0.0.1.*|listen = 0.0.0.0:9000|g" $target
-fi
-
 if [ ! -f "wp-config.php" ]; then
 
 wp core download
 
-#check si le folder wordpress est install
 while [ ! -f "wp-config.php" ]; do
     sleep 1
 done
 
-wp core config --dbname=${MDB_DATABASE} --dbuser=${MDB_USER} --dbpass=${MDB_PASSWORD} --dbhost=${}
+wp core config --dbname="$MDB_DATABASE" --dbuser="$MDB_USER" --dbpass="$MDB_PASSWORD"
 
-# attendre wp-config.php qui est cree donc cheker si bien cree
 while [ ! -f "wp-config-sample.php" ]; do
     sleep 1
 done
 
-wp core install --url="https://${DOMAIN_NAME}" --title=${DOMAIN_NAME} --admin_user=${WP_ADMIN} --admin_password=${WP_ADMIN_PASSWORD} --skip-email
-# wp user create + post
+wp core install --url="https://$DOMAIN_NAME" --title="$DOMAIN_NAME" --admin_user="$WP_ADMIN" --admin_password="$WP_ADMIN_PASSWORD" --skip-email
 
 wp plugin update --all
 
-wp user create moi --user-login=moi --display_name=boss --user_pass=1234 --role=editor
+wp user create moi --user-login="moi" --display_name="boss" --user_pass="1234" --role=editor
 
 fi
 

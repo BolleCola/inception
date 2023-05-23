a0aa54a604b60095d84fbe796155fa640b88d7b4
Thibaud Paquier <TP.Titus@hotmail.fr>
2023-05-23 18:09:16 +0200

    that's work

--- srcs/requirements/wordpress/conf/setup.sh
+++ srcs/requirements/wordpress/conf/setup.sh
@@ -1,5 +1,15 @@
-if [ ! -f "wp-config.php" ]; then
+target="/etc/php7/php-fpm.d/www.conf"
 
+# Check if listen exists in the file
+grep -E "listen = 127.0.0.1" $target >/dev/null 2>&1
+
+# configure www.conf
+if [ $? -eq 0 ]; then
+  sed -i "s|.*listen = 127.0.0.1.*|listen = 0.0.0.0:9000|g" $target
+fi
+
+if [ ! -f "wp-config.php" ]; then
+chmod -R 755 /var/www/*
 wp core download
 
 while [ ! -f "wp-config-sample.php" ]; do
@@ -17,6 +27,14 @@ wp core install --url="https://$DOMAIN_NAME" --title="$DOMAIN_NAME" --admin_user
 wp plugin update --all
 
 wp user create "$WP_USER" "$WP_USER_EMAIL" --role=editor --user_pass="$WP_USER_PASS"
+
+test=0
+
+while [ $test -lt 10 ]
+do
+    test=$((test + 1))
+    wp post create --post_type=post --post_title='ceci est un post tres poster'
+done
 
 fi
 
